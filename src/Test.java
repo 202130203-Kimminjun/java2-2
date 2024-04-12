50d3e7186e11f3af5f78ce406a5b1168fc582b66
202130203-Kimminjun <202130203@email.daelim.ac.kr>
2024-04-12 16:54:29 +0900

    README 파일 업데이트, java 파일 수정

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,95 @@
+class Point {
+    private int x, y;
+    public Point() {
+        this.x = this.y = 0;
+    }
+    public Point(int x, int y) {
+        this.x = x; this.y = y;
+    }
+    public void showPoint() {
+        System.out.println("(" + x + "," + y + ")");
+    }
+}
+
+class ColorPoint extends Point {
+    private String color;
+    public ColorPoint(int x, int y, String color) {
+        super(x, y);
+        this.color = color;
+    }
+    public void showColorPoint() {
+        System.out.print(color);
+        showPoint();
+    }
+}
+
 public class Test {
+    public static void main(String[] args) {
+        ColorPoint cp = new ColorPoint(5, 6, "blue");
+        cp.showColorPoint();
+    }
+}
+        
+
+/*class Point {
+    private int x, y;
+    public void set(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public void showPoint() {
+        System.out.println("(" + x + "," + y + ")");
+    }
+}
+
+class ColorPoint extends Point { 
+    private String color;
+    public void setColor(String color) {
+    this.color = color;
+    }
+    public void showColorPoint() {
+        System.out.print(color);
+        showPoint();
+    }
+}
+
+public class Test {
+    public static void main(String [] args) {
+        Point p = new Point();
+            p.set(1, 2);
+            p.showPoint();
+        ColorPoint cp = new ColorPoint(); 
+            cp.set(3, 4);
+            cp.setColor("red");
+            cp.showColorPoint();
+    }
+}
+    
+
+/*public class Test {
+    public static void main(String[] args) {
+        Calc a = new Calc();
+        int foo = a.n;
+        System.out.println(foo);
+        int bar = Calc.m;
+        System.out.println(bar);
+
+        System.out.println(Calc.abs(-5));
+        System.out.println(Calc.max(10, 8));
+        System.out.println(Calc.min(-3, -8));
+    }
+}
+
+class Calc {
+    int n = 100;
+    static int m = 200;
+
+    public static int abs(int a) { return a>0?a:-a; }
+    public static int max(int a, int b) { return (a>b)?a:b; }
+    public static int min(int a, int b) { return (a>b)?b:a; }
+    }
+
+/*public class Test {
     public static void main(String[] args) {
         Circle [] c; 
         c = new Circle[5];
